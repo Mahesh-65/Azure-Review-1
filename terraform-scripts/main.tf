a64eb9628b52c8dcbfe3ca52ed0e337b0e0fadd9
Mahesh Dineshkumar(UST <307420@ust.com>
2026-05-25 11:38:57 +0530

    Terraform modules

--- terraform-scripts/main.tf
+++ terraform-scripts/main.tf
@@ -7,11 +7,11 @@ module "resource-group" {
 module "virtual-network" {
   source = "./modules/virtual-network"
 
-  resource_group_name = module.rg.resource_group_name
+  resource_group_name = module.resource-group.resource_group_name
   location            = var.location
 
-  vnet_name = var.vnet_name
-  address_space = var.address_space
+  vnet_name      = var.vnet_name
+  address_space  = var.address_space
 
   appgw_subnet_name   = var.appgw_subnet_name
   appgw_subnet_prefix = var.appgw_subnet_prefix
@@ -23,18 +23,18 @@ module "virtual-network" {
 module "network-security-group" {
   source = "./modules/network-security-group"
 
-  resource_group_name = module.rg.resource_group_name
+  resource_group_name = module.resource-group.resource_group_name
   location            = var.location
 
   nsg_name = var.nsg_name
 
-  subnet_id = module.network.vm_subnet_id
+  subnet_id = module.virtual-network.vm_subnet_id
 }
 
 module "app1-vm" {
   source = "./modules/virtual-machine"
 
-  resource_group_name = module.rg.resource_group_name
+  resource_group_name = module.resource-group.resource_group_name
   location            = var.location
 
   vm_name = var.frontend_vm_name
@@ -43,13 +43,13 @@ module "app1-vm" {
   admin_username = var.admin_username
   admin_password = var.admin_password
 
-  subnet_id = module.network.vm_subnet_id
+  subnet_id = module.virtual-network.vm_subnet_id
 }
 
 module "app2-vm" {
   source = "./modules/virtual-machine"
 
-  resource_group_name = module.rg.resource_group_name
+  resource_group_name = module.resource-group.resource_group_name
   location            = var.location
 
   vm_name = var.api_vm_name
@@ -58,25 +58,25 @@ module "app2-vm" {
   admin_username = var.admin_username
   admin_password = var.admin_password
 
-  subnet_id = module.network.vm_subnet_id
+  subnet_id = module.virtual-network.vm_subnet_id
 }
 
 module "nat-gateway" {
   source = "./modules/nat-gateway"
 
-  resource_group_name = module.rg.resource_group_name
+  resource_group_name = module.resource-group.resource_group_name
   location            = var.location
 
   nat_pip_name     = var.nat_pip_name
   nat_gateway_name = var.nat_gateway_name
 
-  subnet_id = module.network.vm_subnet_id
+  subnet_id = module.virtual-network.vm_subnet_id
 }
 
 module "web-application-firewall" {
   source = "./modules/web-application-firewall"
 
-  resource_group_name = module.rg.resource_group_name
+  resource_group_name = module.resource-group.resource_group_name
   location            = var.location
 
   waf_policy_name = var.waf_policy_name
@@ -85,18 +85,18 @@ module "web-application-firewall" {
 module "app-gateway" {
   source = "./modules/app-gateway"
 
-  resource_group_name = module.rg.resource_group_name
+  resource_group_name = module.resource-group.resource_group_name
   location            = var.location
 
   app_gateway_name = var.app_gateway_name
   public_ip_name   = var.appgw_pip_name
 
-  subnet_id = module.network.appgw_subnet_id
+  subnet_id = module.virtual-network.appgw_subnet_id
 
-  frontend_vm_ip = module.frontend_vm.private_ip
-  api_vm_ip      = module.api_vm.private_ip
+  frontend_vm_ip = module.app1-vm.private_ip
+  api_vm_ip      = module.app2-vm.private_ip
 
-  waf_policy_id = module.waf.waf_policy_id
+  waf_policy_id = module.web-application-firewall.waf_policy_id
 
   frontend_domain = var.frontend_domain
   api_domain      = var.api_domain
